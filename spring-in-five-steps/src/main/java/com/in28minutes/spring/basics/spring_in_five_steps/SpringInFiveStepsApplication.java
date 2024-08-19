e4f0effba6ce74a9eae8f88fdb2794046cf64ca3
felipe <felipe@LAPTOP-FELIPE.felipe>
2024-08-18 23:33:44 -0300

    First Commit

---
+++ spring-in-five-steps/src/main/java/com/in28minutes/spring/basics/spring_in_five_steps/SpringInFiveStepsApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.spring_in_five_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInFiveStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInFiveStepsApplication.class, args);
+	}
+
+}
---
+++ spring-in-five-steps/src/test/java/com/in28minutes/spring/basics/spring_in_five_steps/SpringInFiveStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.spring.basics.spring_in_five_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInFiveStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
